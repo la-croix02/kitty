95cd77a18eb87bc73dbb896c29d7a537a00bbde4
Artem <armaksudov1425@gmail.com>
2024-09-28 23:13:57 +0500

    added adaptation

--- js/script.js
+++ js/script.js
@@ -1,34 +1,59 @@
 "use strict";
+window.addEventListener('DOMContentLoaded', () => {
 
-new Swiper('.rooms__slider', {
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true
-    },
-    autoplay: {
-        delay: 4000,
-    },
-    loop: true,
-    grabCursor: true,
-    speed: 800
-});
+    const burgerMenu = document.querySelector('.burger-menu'),
+          menuList = document.querySelector('.header .menu'),
+          body = document.querySelector('body');
+    
+    burgerMenu.addEventListener('click', () => {
+        burgerMenu.classList.toggle('active');
+        menuList.classList.toggle('active');
+        body.classList.toggle('active');
+    })
+    console.log(burgerMenu, menuList)
 
-new Swiper('.reviews__slider', {
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true
-    },
-    loop: false,
-    grabCursor: true,
-    speed: 800,
-    spaceBetween: 30,
-    slidesPerView: 2.5,
+    new Swiper('.rooms__slider', {
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        },
+        autoplay: {
+            delay: 4000,
+        },
+        loop: true,
+        grabCursor: true,
+        speed: 800
+    });
+    
+    new Swiper('.reviews__slider', {
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        },
+        loop: false,
+        grabCursor: true,
+        speed: 800,
+        spaceBetween: 30,
+        slidesPerView: 2.5,
+        breakpoints: {
+            992: {
+                slidesPerView: 2.5
+            },
+            576: {
+                slidesPerView: 1.5
+            },
+            200: {
+                slidesPerView: 1.1
+            }
+        }
+    })
 })
+
